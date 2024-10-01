902edda61eab3d228c71c234cba5201a8ae087b7
Karan PC <prajapatikaran2004@gmail.com>
2024-10-01 23:33:45 +0530

    some changes to the script path and code

---
+++ scripts/create_backend_docs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+JAVADOC_GENERATED_DIR="$FITNESS_FORGE_ROOT_DIR/backend/target/site/apidocs/"
+DOCS_ROOT_DIR="$FITNESS_FORGE_ROOT_DIR/docs"
+
+cd "$FITNESS_FORGE_ROOT_DIR/backend"
+mvn clean
+mvn javadoc:javadoc
+
+cd
+
+rm -rf "$DOCS_ROOT_DIR"
+mv "$JAVADOC_GENERATED_DIR" "$DOCS_ROOT_DIR"
